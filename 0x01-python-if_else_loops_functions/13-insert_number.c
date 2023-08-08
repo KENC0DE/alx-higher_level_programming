b780e09fa2c96f5d494f214795d668e499fddc8d
KENC0DE <kenansameseret@gamil.com>
2023-08-09 05:24:53 +0530

    pypy

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,79 @@
+#include "lists.h"
+
+/**
+ * insert_node - inserts head at appropriet loaction.
+ * @head: head of the linked list.
+ * @number: value to be inserted.
+ *
+ * Return: addres of the new nod if succussful else NULL.
+*/
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *tp;
+	listint_t *new;
+
+	if (!head)
+		return (NULL);
+	if (number < (*head)->n)
+	{
+		new = start_end(head, number, 0);
+		return (new);
+	}
+	tp = *head;
+	while (tp->next)
+	{
+		if (number <= tp->next->n)
+			break;
+		tp = tp->next;
+	}
+	if (!tp)
+		return (NULL);
+
+	if (tp->next == NULL)
+	{
+		new = start_end(&tp, number, 1);
+		return (new);
+	}
+
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
+	new->n = number;
+	new->next = tp->next;
+	tp->next = new;
+	return (new);
+}
+
+/**
+ * start_end - andle when the insertion is at the beginning and end.
+ * @head: head of the linked list.
+ * @num: the data.
+ * @idx: flag.
+ *
+ * Return: the new node.
+*/
+listint_t *start_end(listint_t **head, int num, int idx)
+{
+	listint_t *new;
+
+	if (idx == 0)
+	{
+		new = malloc(sizeof(listint_t));
+		if (!new)
+			return (NULL);
+		new->n = num;
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
+	else
+	{
+		new = malloc(sizeof(listint_t));
+		if (!new)
+			return (NULL);
+		new->n = num;
+		new->next = NULL;
+		(*head)->next = new;
+		return (new);
+	}
+}
