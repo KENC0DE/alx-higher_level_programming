75fdfec2e80b6ee046b2690fa96f352f864ce2f7
KENC0DE <kenansameseret@gamil.com>
2023-08-08 05:46:29 +0530

    py

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -18,21 +18,9 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-/**
- * struct address - structure
- * @p: pointer saved.
- * @next: next pointer.
-*/
-typedef struct address
-{
-	void *p;
-	struct address *next;
-} p_s;
-
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
-void free_p(p_s *head);
 int check_cycle(listint_t *list);
 
 #endif /* LISTS_H */
