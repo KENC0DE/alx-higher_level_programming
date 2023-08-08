f88fa684a6acb10d9de0d32c1e8122b1bd475a58
KENC0DE <kenansameseret@gamil.com>
2023-08-09 03:38:46 +0530

    pypy

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
 def uppercase(str):
-    if str != "":
-        for c in range(len(str) - 1):
-            if ord(str[c]) <= ord('Z'):
-                top = str[c]
-            else:
-                top = chr(ord(str[c]) - 32)
-            print("{}".format(top), end='')
+    if str == "":
+        return
 
-        if ord(str[c + 1]) <= ord('Z'):
-            top = str[c + 1]
+    for c in range(len(str) - 1):
+        if ord(str[c]) <= ord('Z'):
+            top = str[c]
         else:
-            top = chr(ord(str[c + 1]) - 32)
-        print("{}".format(top))
+            top = chr(ord(str[c]) - 32)
+        print("{}".format(top), end='')
 
+    if ord(str[c + 1]) <= ord('Z'):
+        top = str[c + 1]
+    else:
+        top = chr(ord(str[c + 1]) - 32)
+
+    print("{}".format(top))
