caca981a395c1c780bd4a66d45cd39c2cf355b4c
KENC0DE <kenansameseret@gamil.com>
2023-08-11 00:37:48 +0530

    py commits

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,6 +4,8 @@ if __name__ == '__main__':
     argc = len(sys.argv)
     if argc == 1:
         print("0 arguments.")
+    elif argc == 2:
+        print(f"1 argument:\n{argc - 1}: {sys.argv[1]}")
     else:
         print(f"{argc - 1} arguments:")
         for i in range(1, argc):
