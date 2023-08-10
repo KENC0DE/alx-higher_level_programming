574dfb98a4ee5dc39fa28f2cdcdf9c226ceb7069
KENC0DE <kenansameseret@gamil.com>
2023-08-11 00:00:52 +0530

    py commits

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,5 +3,5 @@ if __name__ == '__main__':
     from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
-    print(f"{a} + {b} = {add(a, b)}\n{a} - {b} = {sub(a, b)}")
-    print(f"{a} * {b} = {mul(a, b)}\n{a} / {b} = {div(a, b)}")
+    print(a, "+", b, "=", add(a, b), "\n" + str(a), "-", b, "=", sub(a, b))
+    print(a, "*", b, "=", mul(a, b), "\n" + str(a), "/", b, "=", div(a, b))
