c9236cefd0f7e48a18e3f9e07e1d83745b2d2086
KENC0DE <kenansameseret@gamil.com>
2023-08-11 00:31:32 +0530

    py commits

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,5 +5,6 @@ if __name__ == '__main__':
     if argc == 1:
         print("0 arguments.")
     else:
+        print(f"{argc - 1} arguments:")
         for i in range(1, argc):
             print(f"{i}: {sys.argv[i]}")
