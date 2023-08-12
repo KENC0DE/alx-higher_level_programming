ca47a2c685828d1a8f2eedbede4df9acbadf6ab4
KENC0DE <kenansameseret@gamil.com>
2023-08-12 22:14:01 +0530

    data.py

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,7 +2,7 @@
 def max_integer(my_list=[]):
     if my_list == []:
         return None
-    great = 0
+    great = my_list[0]
     for n in my_list:
         great = great if great > n else n
     return great
