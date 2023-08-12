41297ead0cb76faf3a298b8a3f8bec14b8ac59db
KENC0DE <kenansameseret@gamil.com>
2023-08-12 21:22:26 +0530

    data.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,5 +3,5 @@ def print_matrix_integer(matrix=[[]]):
     if matrix is not None:
         for row in matrix:
             for ln in row:
-                print("{}{}".format(ln, ' ' if ln != row[-1] else ''), end='')
+                print("{:d}".format(ln), end=' ' if ln != row[-1] else '')
             print("{}".format(''))
