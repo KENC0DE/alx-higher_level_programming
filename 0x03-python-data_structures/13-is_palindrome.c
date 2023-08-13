3b938600c829bb0c9d1a7ee5f46316f484b16754
KENC0DE <kenansameseret@gamil.com>
2023-08-13 10:55:19 +0530

    back

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,52 +1,6 @@
 #include "lists.h"
 
 /**
- * check_pali - check for palindrom.
- * @head: head of list.
- * @len: the lenght.
- * Return: 0 or 1.
-*/
-int check_pali(listint_t *head, int len)
-{
-	listint_t *tp;
-	int *nums = malloc(sizeof(int) * (len / 2));
-	int i, half, st_check = 0;
-
-	half = len / 2;
-	i = 0, tp = head;
-	while (tp)
-	{
-		if (i < half && !st_check)
-		{
-			nums[i] = tp->n;
-			i++;
-		}
-		if (i == half)
-		{
-			st_check = 1;
-			if (len % 2 == 0)
-				tp = tp->next;
-			else
-				tp = tp->next->next;
-			i--;
-		}
-
-		if (st_check)
-		{
-			if (nums[i] != tp->n)
-			{
-				free(nums);
-				return (0);
-			}
-			i--;
-		}
-		tp = tp->next;
-	}
-	free(nums);
-	return (1);
-}
-
-/**
  * is_palindrome - check weather is palindrom or not
  * @head: passed linked list head.
  *
@@ -54,23 +8,37 @@ int check_pali(listint_t *head, int len)
 */
 int is_palindrome(listint_t **head)
 {
-	listint_t *tp, *pre = *head;
-	int len = 0;
+	listint_t *back, *front;
 
 	if (!head)
 		return (-1);
 	else if (!(*head))
 		return (1);
-	else if (pre->next == NULL)
-		return (1);
 
-	tp = *head;
-	while (tp)
+	back = *head;
+	front = *head;
+	while (front)
 	{
-		tp = tp->next;
-		len++;
-	}
+		if (front->next != NULL)
+		{
+			if (front->next->next == NULL)
+			{
+				if (back->n == front->next->n)
+				{
+					front->next = NULL;
+					back = back->next;
+					front = back;
+				}
+				else
+					return (0);
+			}
+		}
+		else
+			return (1);
 
-	return (check_pali(*head, len));
+		if (front)
+			front = front->next;
+	}
+	return (1);
 }
 
