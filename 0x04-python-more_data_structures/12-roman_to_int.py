02574c528e1ccd37e4b05e75c94295bd338171dc
KENC0DE <kenansameseret@gamil.com>
2023-08-17 00:29:52 +0530

    py-structure

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python3
-def roman_to_int(roman_string):
+def roman_to_int(rm):
+    if not isinstance(rm, str) or rm is None:
+        return None
     rome = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     num = 0
-    for n in roman_string:
-        num += rome.get(n)
+    leng = len(rm)
+    flag = 1
+    for n in range(leng):
+        if n + 1 < leng:
+            if rome.get(rm[n]) < rome.get(rm[n + 1]):
+                num += rome.get(rm[n + 1]) - rome.get(rm[n])
+                flag = 0
+            else:
+                num += rome.get(rm[n])
+                flag = 1
+        elif flag:
+            num += rome.get(rm[n])
     return num
