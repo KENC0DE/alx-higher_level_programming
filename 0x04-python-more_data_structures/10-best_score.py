890b4c7cf883a2206e1f38a2a7202e550afa2509
KENC0DE <kenansameseret@gamil.com>
2023-08-16 21:24:43 +0530

    py-structure

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def best_score(a_dic):
+    if a_dic is None:
+        return None
+    scr = 0
+    track = 0
+    for c in a_dic:
+        scr = scr if scr > a_dic[c] else a_dic[c]
+        if track != scr:
+            name = c
+            track = scr
+    return name
