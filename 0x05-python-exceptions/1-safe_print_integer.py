1d571c002da9685f9b5a71dacd92f0e28721feea
KENC0DE <kenansameseret@gamil.com>
2023-08-28 19:29:27 +0530

    exception

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    try:
-        print("{:d}".format(value))
-        return True
-    except ValueError:
-        return False
+    if value is not None:
+        try:
+            print("{:d}".format(value))
+            return True
+        except ValueError:
+            return False
+    else:
+        return None
