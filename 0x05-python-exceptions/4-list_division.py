f4480d0ec7f3dfb1cdc788bf8484d654ea43680b
KENC0DE <kenansameseret@gamil.com>
2023-08-28 20:19:50 +0530

    exception

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,17 @@
+def list_division(my_list_1, my_list_2, list_length):
+    div_ls = []
+    for i in range(list_length):
+        try:
+            val = my_list_1[i] / my_list_2[i]
+        except TypeError:
+            print("wrong type")
+            val = 0
+        except ZeroDivisionError:
+            print("division by 0")
+            val = 0
+        except IndexError:
+            print("out of range")
+            val = 0
+        finally:
+            div_ls.append(val)
+    return div_ls
