1bd6ceaca782451c02527a52dd137966eab513dc
KENC0DE <kenansameseret@gamil.com>
2023-08-28 19:26:27 +0530

    exception

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    istf = True
     try:
         print("{:d}".format(value))
+        return True
     except ValueError:
-        istf = False
-    return istf
+        return False
