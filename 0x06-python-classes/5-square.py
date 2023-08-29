9f4d5ef7cc2e6da2a19efeb677af14e20b30e3d5
KENC0DE <kenansameseret@gamil.com>
2023-08-30 03:13:35 +0530

    py OPP

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Class with prinvate attribut and pre-check assignment"""
+
+
+class Square:
+    """Initializer"""
+    def __init__(self, size=0):
+        if isinstance(size, int) is False:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int) is False:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size != 0:
+            for sq in range(self.__size):
+                print("#" * self.__size)
+        else:
+            print()
