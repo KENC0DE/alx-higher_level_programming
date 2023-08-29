27a040a454b6d2fc91fec3cd004cc976cd788022
KENC0DE <kenansameseret@gamil.com>
2023-08-30 05:27:57 +0530

    py OPP

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
+"""Class with prinvate attribut and pre-check assignment"""
 
 
 class Square:
     """Initializer"""
     def __init__(self, size=0, position=(0, 0)):
-        if isinstance(size, int) is False:
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
-            self.__position = position
+        self.__size = size
+        self.__position = position
 
     @property
     def size(self):
