ffa73bb2462b0a7ea85dc4adfc09707756b9784a
KENC0DE <kenansameseret@gamil.com>
2023-08-30 05:47:09 +0530

    py OPP

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
-"""Class with prinvate attribut and pre-check assignment"""
+"""Defines a class Square"""
 
 
 class Square:
-    """Initializer"""
+    """Represents a square"""
     def __init__(self, size=0, position=(0, 0)):
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
+
+    def area(self):
+        return (self.__size) ** 2
 
     @property
     def size(self):
@@ -14,36 +17,33 @@ class Square:
 
     @size.setter
     def size(self, value):
-        if isinstance(value, int) is False:
+        if type(value) is not int:
             raise TypeError("size must be an integer")
-        elif value < 0:
-            raise ValueError("size must be >= 0")
         else:
-            self.__size = value
-
-    def area(self):
-        return self.__size ** 2
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = value
 
     def my_print(self):
-        if self.__size != 0:
-            if self.__position[1] > 0:
-                print("\n" * (self.__position[1] - 1))
-            for sq in range(self.__size):
-                print(" " * self.__position[0] + "#" * self.__size)
-        else:
+        if self.__size == 0:
             print()
+            return
+        for i in range(self.__position[1]):
+            print()
+        for j in range(self.__size):
+            print("".join([" " for k in range(self.__position[0])]), end="")
+            print("".join(["#" for z in range(self.__size)]))
 
     @property
     def position(self):
-        """Nothing to do"""
+        return self.__position
 
     @position.setter
     def position(self, value):
-        if (not isinstance(value, tuple) or
-                len(value) != 2 or
-                not isinstance(value[0], int) or
-                not isinstance(value[1], int) or
-                value[0] < 0 or value[1] < 0):
+        if type(value) is not tuple or len(value) != 2 or \
+           type(value[0]) is not int or value[0] < 0 or \
+           type(value[1]) is not int or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
