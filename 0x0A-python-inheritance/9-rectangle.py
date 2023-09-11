a79cc187e47490ca40f2e40411f2ba4cd1fdaf86
KENC0DE <kenansameseret@gmail.com>
2023-09-12 04:36:58 +0530

    inherit

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-"""
-9-rectangle module
-"""
+""" Rectangle """
+
+
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """build rect ...  """
+    """ Inheritor class """
+
     def __init__(self, width, height):
-        """ init """
+        """ initializor. """
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
@@ -18,4 +19,4 @@ class Rectangle(BaseGeometry):
         return self.__width * self.__height
 
     def __str__(self):
-        return str("[Rectangle] {}/{}".format(self.__width, self.__height))
+        return "[Rectangel] {}/{}".format(self.__width, self.__height)
