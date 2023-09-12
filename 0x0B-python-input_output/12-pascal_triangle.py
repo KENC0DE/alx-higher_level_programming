1cb757f8d94766bb38bfa8916329dc5399b730b4
KENC0DE <kenansameseret@gmail.com>
2023-09-13 03:49:16 +0530

    Python I/O

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -6,26 +6,22 @@
 
 def pascal_triangle(n):
     """ Creates pascal triangle """
-    p_triangle = []
+    p_triangle = [[1], [1, 1]]
     cr = 1
     if n <= 0:
         return p_triangle
+    if n == 1:
+        return p_triangle[:-1]
     for i in range(2, n):
-        if len(p_triangle) < 1:
-            p_triangle.append([1])
-            if n >= 2:
-                p_triangle.append([1, 1])
+        tp = p_triangle[cr]
+        cr += 1
 
-        if n >= 3:
-            tp = p_triangle[cr]
-            cr += 1
-
-            tmp_ls = [1]
-            for j in range(len(tp)):
-                if j + 1 < len(tp):
-                    tmp_ls.append(tp[j] + tp[j + 1])
-                else:
-                    tmp_ls.append(1)
-                    p_triangle.append(tmp_ls)
-                    break
+        tmp_ls = [1]
+        for j in range(len(tp)):
+            if j + 1 < len(tp):
+                tmp_ls.append(tp[j] + tp[j + 1])
+            else:
+                tmp_ls.append(1)
+                p_triangle.append(tmp_ls)
+                break
     return p_triangle
