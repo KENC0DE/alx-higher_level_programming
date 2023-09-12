765b0fa86db881e8ce8b65aebf44b5df6ccba8a8
KENC0DE <kenansameseret@gmail.com>
2023-09-12 20:36:42 +0530

    Python I/O

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+    append to a file
+"""
+
+
+def append_write(filename="", text=""):
+    with open(filename, 'a', encoding='utf-8') as af:
+        appended = af.write(text)
+    return appended
