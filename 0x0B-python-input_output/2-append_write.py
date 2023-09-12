c9d89bf3dc2711def40592529accb1a39526e80b
KENC0DE <kenansameseret@gmail.com>
2023-09-12 20:41:12 +0530

    Python I/O

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -5,6 +5,7 @@
 
 
 def append_write(filename="", text=""):
+    """ appens to a file or create if it doesn't exists """
     with open(filename, 'a+', encoding='utf-8') as af:
         appended = af.write(text)
     return appended
