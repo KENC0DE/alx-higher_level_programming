a1269cc03fb9e8d15715493da0ebb4388edd958f
KENC0DE <kenansameseret@gmail.com>
2023-09-13 00:11:56 +0530

    Python I/O

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -10,17 +10,9 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-def arg_to_list(f_name):
+def arg_to_list():
     """ Configure JSON """
-    obj_form = load_from_json_file(f_name)
-    save_to_json_file(obj_form, f_name)
-
-
-def main():
-    """ execution point """
     f_name = "add_item.json"
     save_to_json_file(sys.argv[1:], f_name)
-    arg_to_list(f_name)
 
-
-main()
+arg_to_list()
