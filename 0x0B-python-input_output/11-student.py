7d07b5cb4f645a6e4a4b74b49ba704e3f24e3a5c
KENC0DE <kenansameseret@gmail.com>
2023-09-13 02:29:31 +0530

    Python I/O

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+    Play with student
+"""
+
+
+class Student:
+    """ Student class """
+
+    def __init__(self, f_name, l_name, age):
+        self.first_name = f_name
+        self.last_name = l_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        fdi = self.__dict__
+        if attrs is None:
+            return fdi
+        emp = {x: fdi[x] for x in attrs if x in fdi}
+        return emp
+
+    def reload_from_json(self, json):
+        self.first_name = json['first_name']
+        self.last_name = json['last_name']
+        self.age = json['age']
