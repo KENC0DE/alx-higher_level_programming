a00f74723ee3d972b8e68ba851d8deb9d87f8cb2
KENC0DE <kenansameseret@gmail.com>
2023-09-12 19:48:42 +0530

    Python I/O

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+"""
+    Write into a file
+"""
+
+
+def write_file(filename="", text=""):
+    """ writes into a fle func """
+    with open(filename, "w", encoding='utf-8') as sf:
+        what_written = sf.write(text)
+    return what_written
