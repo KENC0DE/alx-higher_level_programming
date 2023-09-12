cd18b1880a2941d6033552105f7b7fa7ca364a87
KENC0DE <kenansameseret@gmail.com>
2023-09-12 05:45:53 +0530

    inherit

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -6,6 +6,6 @@
 
 def add_attribute(obj, name, a_name):
     """ add attribute """
-    if type(obj) is str:
+    if type(obj) is str or type(obj) is tuple:
         raise TypeError("can't add new attribute")
     obj.name = a_name
