6357e2666fd5c543fe8da32cecf2891e939a2ac8
KENC0DE <kenansameseret@gmail.com>
2023-09-12 19:25:05 +0530

    Python I/O

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,5 +6,5 @@
 
 def read_file(filename=""):
     """ Read file """
-    with open(filename, encoding='utf-8') as fp:
+    with open(filename, 'r', encoding='utf-8') as fp:
         print(fp.read())
