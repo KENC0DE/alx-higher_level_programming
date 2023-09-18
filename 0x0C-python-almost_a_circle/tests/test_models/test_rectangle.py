fd138245036bfb3492a7e192e012c39e36d290b9
KENC0DE <kenansameseret@gmail.com>
2023-09-18 21:58:58 +0530

    tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,29 +1,126 @@
 #!/usr/bin/python3
 """
-    Test Rectangle with Unit Test
+rectangle module
 """
+from models.base import Base
 
 
-import unittest
-from models.rectangle import Rectangle
+class Rectangle(Base):
+    """
+    Rectangle implementation
+    """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        init - initialization
+        Args:
+            width: width of the rectangle
+            height: height of the rectangle
+            x: x position
+            y: y position
+            id: object id
+        """
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
+    def __str__(self):
+        """
+        overriden __str__ method
+        """
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(
+                self.id, self.x, self.y, self.width, self.height)
 
-class TestRectangle(unittest.TestCase):
-    """ Test Rectangle better tested. """
+    def check(self, key, value, less_eq=True):
+        """
+        checks for value and type error
+        """
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(key))
+        if less_eq:
+            if value <= 0:
+                raise ValueError("{} must be > 0".format(key))
+        else:
+            if value < 0:
+                raise ValueError("{} must be >= 0".format(key))
 
-    def setUp(self):
-        Rectangle._Base__nb_objects = 0
+    @property
+    def width(self):
+        """ width getter """
+        return self.__width
 
-    def test_0(self):
-        r1 = Rectangle(10, 2)
-        self.assertEqual(r1.id, 1)
+    @width.setter
+    def width(self, width):
+        """ width setter """
+        self.check('width', width)
+        self.__width = width
 
-        r2 = Rectangle(2, 10)
-        self.assertEqual(r2.id, 2)
+    @property
+    def height(self):
+        """ height getter """
+        return self.__height
 
-        r3 = Rectangle(10, 2, 0, 0, 12)
-        self.assertEqual(r3.id, 12)
+    @height.setter
+    def height(self, height):
+        """ height setter """
+        self.check('height', height)
+        self.__height = height
 
+    @property
+    def x(self):
+        """ x getter """
+        return self.__x
 
-if __name__ == '__main__':
-    unittest.main()
+    @x.setter
+    def x(self, x):
+        """ x setter """
+        self.check('x', x, False)
+        self.__x = x
+
+    @property
+    def y(self):
+        """ y getter """
+        return self.__y
+
+    @y.setter
+    def y(self, y):
+        """ y setter """
+        self.check('y', y, False)
+        self.__y = y
+
+    def area(self):
+        """ area """
+        return self.__width * self.__height
+
+    def display(self):
+        """
+        prints rectangle
+        """
+        for y in range(self.y):
+            print()
+        for column in range(self.height):
+            for x in range(self.x):
+                print(' ', end='')
+            for row in range(self.width):
+                print("#", end='')
+            print()
+
+    def update(self, *args, **kwargs):
+        """
+        assigns an argument to each attribute
+        """
+        lst = (self.id, self.width, self.height, self.x, self.y)
+        if args:
+            self.id, self.width, self.height, self.x, self.y = \
+                    args + lst[len(args):len(lst)]
+        elif kwargs:
+            for (key, value) in kwargs.items():
+                setattr(self, key, value)
+
+    def to_dictionary(self):
+        """
+        returns the dictionary representation of a Rectangle.
+        """
+        return {'x': self.x, 'y': self.y, 'id': self.id,
+                'height': self.height, 'width': self.width}
--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -1,27 +1,57 @@
 #!/usr/bin/python3
 """
-    Test Square
+square module
 """
-import unittest
-from models.square import Square
+from models.rectangle import Rectangle
 
 
-class TestSquare(unittest.TestCase):
-    """ Test Special Rectangle """
+class Square(Rectangle):
+    """
+    Square implementation
+    """
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+        init - initilizer
+        Args:
+            size: size of the square
+            x: x position of the square
+            y: y position of the square
+            id: id of the square
+        """
+        super().__init__(size, size, x, y, id)
 
-    def setUp(self):
-        Square._Base__nb_objects = 0
+    def __str__(self):
+        """
+        overriden __str__ method
+        """
+        return "[Square] ({}) {}/{} - {}".format(
+                self.id, self.x, self.y, self.width)
 
-    def test_0(self):
-        s1 = Square(5)
-        self.assertEqual(s1.area(), 25)
+    @property
+    def size(self):
+        """size getter"""
+        return self.width
 
-        s2 = Square(2, 2)
-        self.assertEqual(s2.area(), 4)
+    @size.setter
+    def size(self, size):
+        """size setter"""
+        self.width = size
+        self.height = size
 
-        s3 = Square(3, 1, 3)
-        self.assertEqual(s3.area(), 9)
+    def update(self, *args, **kwargs):
+        """
+        assigns attributes
+        """
+        lst = (self.id, self.size, self.x, self.y)
+        if args:
+            self.id, self.size, self.x, self.y = \
+                    args + lst[len(args):len(lst)]
+        elif kwargs:
+            for (key, value) in kwargs.items():
+                setattr(self, key, value)
 
-
-if __name__ == '__main__':
-    unittest.main()
+    def to_dictionary(self):
+        """
+        that returns the dictionary representation of a Square.
+        """
+        return {'id': self.id, 'x': self.x, 'size': self.size, 'y': self.y}
