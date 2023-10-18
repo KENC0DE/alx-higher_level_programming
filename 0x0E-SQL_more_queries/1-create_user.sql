5198edbfb40b8d81c1a2ca7be23996147291abc7
KENC0DE <kenansameseret@gmail.com>
2023-10-18 21:49:02 +0530

    mariadb

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- Create User, Grant all Privileges, asing password
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost;
