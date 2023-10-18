732a63eeb3a2e1aca5d4e7d05ff534032a626306
KENC0DE <kenansameseret@gmail.com>
2023-10-18 23:57:40 +0530

    mariadb

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,5 @@
 -- Cities of California
-SELECT id, name FROM cities WHERE state_id = 1 ORDER BY id ASC;
+SELECT id, name FROM cities WHERE state_id = (
+SELECT id FROM states WHERE name = 'California')
+ORDER BY id ASC;
 
