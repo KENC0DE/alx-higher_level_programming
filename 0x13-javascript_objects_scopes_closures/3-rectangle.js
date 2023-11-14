2403c123f4090461c8d0e02332cef9dc903d5101
KENC0DE <kenansameseret@gmail.com>
2023-11-15 01:09:28 +0530

    js

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let h = 0; h < this.height; h++) {
+      let ln = '';
+      for (let w = 0; w < this.width; w++) {
+        ln += 'X';
+      }
+      console.log(ln);
+    }
+  }
+};
