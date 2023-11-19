53ab92e1634aa4268df9eb484c657c8ef07ded4d
KENC0DE <kenansameseret@gmail.com>
2023-11-20 04:20:59 +0530

    ORM

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" First state model
+"""
+
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+
+class State(Base):
+    """ State clase to table"""
+    __table_name__ = 'states'
+    id = Column(Integer, autoincreament=True, unqiue=True,
+                nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
