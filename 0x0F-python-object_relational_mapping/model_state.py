87ad625873ece647c4340d2ce8234be5b91fc1e9
KENC0DE <kenansameseret@gmail.com>
2023-11-20 04:23:03 +0530

    ORM

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -11,7 +11,7 @@ Base = declarative_base()
 
 class State(Base):
     """ State clase to table"""
-    __table_name__ = 'states'
+    __tablename__ = 'states'
     id = Column(Integer, autoincreament=True, unqiue=True,
                 nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
