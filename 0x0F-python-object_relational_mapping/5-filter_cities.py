8c08958b4e32bb9e14ca3021c50b6fe06c2b3090
KENC0DE <kenansameseret@gmail.com>
2023-11-20 02:50:43 +0530

    ORM

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -14,7 +14,8 @@ if __name__ == '__main__':
     cursor = conn.cursor()
 
     query = """SELECT cities.name FROM cities
-                WHERE cities.state_id = (SELECT id FROM states WHERE name = %s)
+                INNER JOIN states ON cities.state_id = states.id
+                WHERE states.name = %s
                 ORDER BY cities.id ASC"""
     cursor.execute(query, (sys.argv[4],))
 
