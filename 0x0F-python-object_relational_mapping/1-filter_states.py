ce05321d0ceaabc87dc179904e98b516a7cf6677
KENC0DE <kenansameseret@gmail.com>
2023-11-20 00:44:57 +0530

    ORM

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+    1. Filter states
+"""
+
+
+if __name__ == '__main__':
+    import sys
+    import MySQLdb
+
+    conn = MySQLdb.connect(host='localhost', port=3306,
+                           user=sys.argv[1], passwd=sys.argv[2],
+                           db=sys.argv[3])
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM states WHERE name LIKE "N%" ORDER BY id ASC')
+    rec = cursor.fetchall()
+
+    for r in rec:
+        print(r)
