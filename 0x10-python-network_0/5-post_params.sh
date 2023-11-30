c6c5dafa131445c12cfbea83a44bd9b569b40143
KENC0DE <kenansameseret@gmail.com>
2023-12-01 01:25:45 +0530

    http hmm...

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # displays the body of the response
-curl -sH "email: test@gmail.com" -H "subject: I will always be here for PLD" -X POST "$1"
+curl -s -d "email=test@gmail.com&subject=I will always be here for PLD" -X POST "$1"
