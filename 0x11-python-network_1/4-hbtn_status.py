65af80f8e890c1c50f3008b0058d2e7ce51a4b09
KENC0DE <kenansameseret@gmail.com>
2023-12-02 01:17:44 +0530

    rqst

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""
+    fetches https://alx-intranet.hbtn.io/status
+"""
+
+
+import requests
+
+if __name__ == '__main__':
+    url = "https://alx-intranet.hbtn.io/status"
+
+    got = requests.get(url)
+
+    print('Body response:')
+    print(f'\t- type: {type(got.headers["content-type"])}')
+    print(f'\t- content: {got.text}')
