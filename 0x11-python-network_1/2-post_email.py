99513eceacac7c1d38c69eb58dfb9699492675e6
KENC0DE <kenansameseret@gmail.com>
2023-12-02 00:42:19 +0530

    rqst

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+"""
+    sends a POST request to the passed URL
+    with the email as a parameter, and displays
+    the body of the response (decoded in utf-8)
+"""
+
+
+import urllib.request
+import urllib.parse
+import sys
+
+if __name__ == '__main__':
+    params = {'email': sys.argv[2]}
+    url = sys.argv[1]
+    
+    data = urllib.parse.urlencode(params)
+    data = data.encode('utf-8')
+    
+    req = urllib.request.Request(url, data, method='POST')
+    with urllib.request.urlopen(req) as resp:
+        got = resp.read()
+        dcd = got.decode('utf-8')
+
+    print(dcd)
