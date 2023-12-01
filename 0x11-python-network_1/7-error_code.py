b505d2dc5a79a8a7bc901f2bbee8b647e4a138b5
KENC0DE <kenansameseret@gmail.com>
2023-12-02 01:39:31 +0530

    rqst

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""
+    sends a request to the URL and
+    displays the body of the response.
+"""
+
+
+import requests
+import sys
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+
+    resp = requests.get(url)
+    if resp.status_code >= 400:
+        print(f'Error code: {resp.status_code}')
+    else:
+        print(resp.text)
