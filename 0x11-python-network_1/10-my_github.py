08e30875e35f611bf17270a98172507486ab0f17
KENC0DE <kenansameseret@gmail.com>
2023-12-02 02:21:11 +0530

    rqst

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""
+    takes your GitHub credentials (username and password)
+    and uses the GitHub API to display your id
+"""
+
+
+import requests
+import sys
+
+if __name__ == '__main__':
+    url = "http://api.github.com/user"
+
+    resp = requests.get(url, auth=(sys.argv[1], sys.argv[2]))
+    print(resp.json().get('id'))
