264af541dc7b846b7992ecac7bcd31d09be960c8
KENC0DE <kenansameseret@gmail.com>
2023-12-02 01:21:13 +0530

    rqst

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""
+    sends a request to the URL and displays
+    the value of the variable X-Request-Id in
+    the response header
+"""
+
+
+import requests
+import sys
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+
+    got = requests.get(url)
+
+    print(f"{got.headers['X-Request-Id']}")
