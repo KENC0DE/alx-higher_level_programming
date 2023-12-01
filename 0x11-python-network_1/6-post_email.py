a214952a6efffc15d70af7f083ec9219752a4485
KENC0DE <kenansameseret@gmail.com>
2023-12-02 01:32:43 +0530

    rqst

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""
+    sends a POST request to the passed URL
+    with the email as a parameter, and finally
+    displays the body of the response.
+"""
+
+
+import requests
+import sys
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    val = {'email': sys.argv[2]}
+
+    got = requests.post(url, data=val)
+
+    print(got.text)
