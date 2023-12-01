50f62cac0cb4040c8c207b0f3d22b57ec475a9e3
KENC0DE <kenansameseret@gmail.com>
2023-12-02 00:45:33 +0530

    rqst

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -13,10 +13,10 @@ import sys
 if __name__ == '__main__':
     params = {'email': sys.argv[2]}
     url = sys.argv[1]
-    
+
     data = urllib.parse.urlencode(params)
     data = data.encode('utf-8')
-    
+
     req = urllib.request.Request(url, data, method='POST')
     with urllib.request.urlopen(req) as resp:
         got = resp.read()
