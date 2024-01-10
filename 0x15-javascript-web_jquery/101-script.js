dea4163091d7252f375093d4697acec9965aa5b8
KENC0DE <kenansameseret@gmail.com>
2024-01-11 01:56:44 +0530

    jquery

---
+++ 0x15-javascript-web_jquery/101-script.js
@@ -0,0 +1,13 @@
+window.onload = () => {
+  $('div#add_item').click(() => {
+    $('ul.my_list').append('<li>Item</li>');
+  });
+
+  $('div#remove_item').click(() => {
+    $('ul.my_list li:last').remove();
+  });
+
+  $('div#clear_list').click(() => {
+    $('ul.my_list').empty();
+  });
+};
